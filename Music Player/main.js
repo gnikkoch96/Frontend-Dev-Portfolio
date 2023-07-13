9443408f409c539f8418bd9a96a267030d3a697e
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2023-07-12 19:14:47 -0700

    finished music player project

---
+++ Music Player/main.js
@@ -0,0 +1,116 @@
+// declare const vars to reference html elements
+const image = document.getElementById('cover'),
+    title = document.getElementById('music-title'),
+    artist = document.getElementById('music-artist'),
+    currentTimeEl = document.getElementById('current-time'),
+    durationEl = document.getElementById('duration'),
+    progress = document.getElementById('progress'),
+    playerProgress = document.getElementById('player-progress'),
+    prevBtn = document.getElementById('prev'),
+    nextBtn = document.getElementById('next'),
+    playBtn = document.getElementById('play'),
+    background = document.getElementById('bg-img');
+
+
+// uses js built in audio library
+const music = new Audio();
+
+// contains an array of objects
+const songs = [
+    {
+        path: 'assets/1.mp3',
+        displayName: 'Instant Crush',
+        cover: 'assets/1.png',
+        artist: 'Youtube'
+    },
+    {
+        path: 'assets/2.mp3',
+        displayName: 'Tropic Fuse',
+        cover: 'assets/2.png',
+        artist: 'Youtube'
+    },
+    {
+        path: 'assets/3.mp3',
+        displayName: 'Wind Riders',
+        cover: 'assets/3.png',
+        artist: 'Youtube'
+    }
+]
+
+let musicIndex = 0;
+let isPlaying = false;
+
+function togglePlay(){
+    if(isPlaying){
+        pauseMusic();
+    }else{
+        playMusic();
+    }
+}
+
+function playMusic(){
+    isPlaying = true;
+
+    // change the play button icon
+    playBtn.classList.replace('fa-play', 'fa-pause');
+
+    // set button hover title
+    playBtn.setAttribute('title', 'Pause');
+    music.play();
+}
+
+
+function pauseMusic(){
+    isPlaying = false;
+
+    // change the play button icon
+    playBtn.classList.replace('fa-pause', 'fa-play');
+
+    // set button hover title
+    playBtn.setAttribute('title', 'Play');
+    music.pause();
+}
+
+function loadMusic(musicIndex){
+    let song = songs[musicIndex];
+
+    music.src = song.path;
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    image.src = song.cover;
+    background.src = song.cover;
+}
+
+function changeMusic(direction){
+    musicIndex = (musicIndex + direction + songs.length) % songs.length;
+    loadMusic(musicIndex);
+    playMusic();
+}
+
+function updateProgressBar(){
+    const {duration, currentTime} = music;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+
+    // pads the time to the format 00
+    const formatTime = (time) => String(Math.floor(time)).padStart(2, '0');
+
+    durationEl.textContent = `${formatTime(duration /60)}:${formatTime(duration % 60)}`;
+
+    currentTimeEl.textContent = `${formatTime(currentTime / 60)}: ${formatTime(currentTime % 60)}`;
+}
+
+function setProgressBar(e){
+    const width = playerProgress.clientWidth;
+    const clickX = e.offsetX;
+    music.currentTime = (clickX / width) * music.duration;
+}
+
+playBtn.addEventListener('click', togglePlay);
+prevBtn.addEventListener('click', () => changeMusic(-1));
+nextBtn.addEventListener('click', () => changeMusic(1));
+music.addEventListener('ended', () => changeMusic(1));
+music.addEventListener('timeupdate', updateProgressBar);
+playerProgress.addEventListener('click', setProgressBar);
+
+loadMusic(musicIndex);
